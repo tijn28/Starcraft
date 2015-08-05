1ac142acdf6d0b5c98ce85c8e58dd5a4d0a4fc1b
HarmGriffioen <h.j.griffioen@student.tudelft.nl>
2015-08-05 11:27:31 +0200

    Fixed stimpacks ability for marines, the academy is not yet researching the stim packs

--- eisbw/src/main/java/eisbw/actions/Use.java
+++ eisbw/src/main/java/eisbw/actions/Use.java
@@ -2,9 +2,12 @@ package eisbw.actions;
 
 import eis.exceptions.ActException;
 import eis.iilang.*;
+
 import java.util.LinkedList;
+
 import jnibwapi.*;
 import jnibwapi.types.TechType;
+import jnibwapi.types.TechType.TechTypes;
 
 public class Use extends StarcraftAction {
 
@@ -15,7 +18,6 @@ public class Use extends StarcraftAction {
     @Override
     public boolean isValid(Action action) {
         LinkedList<Parameter> parameters = action.getParameters();
-
         if (parameters.size() == 1) { // type
             boolean isTechType = parameters.get(0) instanceof Identifier && utility.getTechType(((Identifier) parameters.get(0)).getValue()) != null;
             return isTechType;
@@ -36,11 +38,11 @@ public class Use extends StarcraftAction {
     public void execute(Unit unit, Action action) throws ActException {
         LinkedList<Parameter> parameters = action.getParameters();
         TechType techType = utility.getTechType(((Identifier) parameters.get(0)).getValue());
-        api.useTech(unit.getTypeID(), techType.getID());
+        unit.useTech(techType);
     }
 
     @Override
     public String toString() {
-        return "use(unitId, techId)";
+        return "use(techId)";
     }
 }
