4bd64f7f043374ecb652e126a05ed55f6251bcde
HarmGriffioen <h.j.griffioen@student.tudelft.nl>
2015-09-22 16:56:20 +0200

    Added Mapdata
    Added new maps, there are now also 3 and 4 player maps, maps can be
    changed from the chaoslauncher or selected manually.
    
    Maps:
    	* (2)Benzene
    	* (2)Destination
    	* (2)Heartbreak Ridge
    	* (3)Neo Moon Glaive
    	* (3)Tau Cross
    	* (4)Andromeda
    	* (4)Circuit Breaker
    	* (4)Electric Circuit
    	* (4)Empire of the Sun
    	* (4)Fighting Spirit
    	* (4)Icarus
    	* (4)Jade
    	* (4)La Mancha1.1
    	* (4)Python
    	* (4)Roadrunner

---
+++ eisbw/src/main/java/eisbw/actions/Skip.java
@@ -0,0 +1,37 @@
+package eisbw.actions;
+
+import java.util.LinkedList;
+
+import jnibwapi.JNIBWAPI;
+import jnibwapi.Unit;
+import eis.exceptions.ActException;
+import eis.iilang.Action;
+import eis.iilang.Parameter;
+
+public class Skip extends StarcraftAction {
+
+    public Skip(JNIBWAPI api) {
+        super(api);
+    }
+
+    @Override
+    public boolean isValid(Action action) {
+        LinkedList<Parameter> parameters = action.getParameters();
+        return (parameters.size() == 0);
+    }
+
+    @Override
+    public boolean canExecute(Unit unit, Action action) {
+        return true;
+    }
+
+    @Override
+    public void execute(Unit unit, Action action) throws ActException {
+    	
+    }
+
+    @Override
+    public String toString() {
+        return "skip";
+    }
+}
