9bd941614cd24def9109f2f4a1ab28fdab32508d
HarmGriffioen <harmgriffioen@gmail.com>
2016-05-06 11:00:37 +0200

    Added first tests

---
+++ eisbw/src/main/test/eisbw/percepts/percievers/AddonPercieverTest.java
@@ -0,0 +1,42 @@
+package eisbw.percepts.percievers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import eisbw.percepts.perceivers.AddonPerceiver;
+import jnibwapi.Unit;
+import jnibwapi.types.UnitType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class AddonPercieverTest {
+
+  private AddonPerceiver addonPerciever;
+  @Mock
+  private Unit unit;
+  @Mock
+  private UnitType unittype;
+
+  /**
+   * Initialize mocks.
+   */
+  @Before
+  public void start() {
+    MockitoAnnotations.initMocks(this);
+    when(unit.getAddon()).thenReturn(unit);
+    when(unit.getType()).thenReturn(unittype);
+    when(unittype.getName()).thenReturn("unitType");
+    addonPerciever = new AddonPerceiver(null, unit);
+  }
+  
+  @Test
+  public void test() {
+    assertEquals("addon", addonPerciever.perceive().get(0).getName());
+    assertEquals("unitType",
+        addonPerciever.perceive().get(0).getParameters().get(0).toProlog());
+  }
+
+}
