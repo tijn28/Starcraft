853fa326a54126fc042101ea2a507e265baaa231
HarmGriffioen <harmgriffioen@gmail.com>
2016-05-09 14:21:08 +0200

    Started adding action tests.

--- eisbw/src/test/java/eisbw/actions/AttackMoveTest.java
+++ eisbw/src/test/java/eisbw/actions/AttackMoveTest.java
@@ -11,6 +11,7 @@ import eis.iilang.Identifier;
 import eis.iilang.Numeral;
 import eis.iilang.Parameter;
 import jnibwapi.JNIBWAPI;
+import jnibwapi.Position;
 import jnibwapi.Unit;
 import jnibwapi.types.UnitType;
 
@@ -21,9 +22,9 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.LinkedList;
 
-public class AttackTest {
+public class AttackMoveTest {
 
-  private Attack action;
+  private AttackMove action;
   private LinkedList<Parameter> params;
 
   @Mock
@@ -41,11 +42,12 @@ public class AttackTest {
   @Before
   public void start() {
     MockitoAnnotations.initMocks(this);
-    action = new Attack(bwapi);
-
+    action = new AttackMove(bwapi);
+    
     params = new LinkedList<>();
     params.add(new Numeral(1));
-
+    params.add(new Numeral(2));
+    
     when(act.getParameters()).thenReturn(params);
     when(unit.getType()).thenReturn(unitType);
   }
@@ -56,27 +58,37 @@ public class AttackTest {
     params.set(0, new Identifier("Not Working"));
     assertFalse(action.isValid(act));
     params.set(0, new Numeral(1));
+    params.set(1, new Identifier("Not Working"));
+    assertFalse(action.isValid(act));
+    params.set(1, new Numeral(2));
     params.add(new Numeral(10));
     assertFalse(action.isValid(act));
   }
-
+  
   @Test
   public void canExecute_test() {
+    when(unitType.isAttackCapable()).thenReturn(false);
+    when(unitType.isCanMove()).thenReturn(false);
+    assertFalse(action.canExecute(unit, act));
+    when(unitType.isCanMove()).thenReturn(true);
+    assertFalse(action.canExecute(unit, act));
     when(unitType.isAttackCapable()).thenReturn(true);
     assertTrue(action.canExecute(unit, act));
+    when(unitType.isCanMove()).thenReturn(false);
+    assertFalse(action.canExecute(unit, act));
   }
-
+  
   @Test
   public void execute_test() {
     when(bwapi.getUnit(1)).thenReturn(unit);
     when(unitType.isAttackCapable()).thenReturn(true);
     action.execute(unit, act);
-    verify(unit).attack(unit, false);
+    verify(unit).attack(new Position(1, 2, Position.PosType.BUILD), false);
   }
-
+  
   @Test
   public void toString_test() {
-    assertEquals("attack(targetId)", action.toString());
+    assertEquals("attack(x, y)", action.toString());
   }
 
 }
