33cefe552d3b56f20c1bd1381f186f410c369349
Dahny <P.C.plenge@student.tudelft.nl>
2016-05-17 12:48:16 +0200

    updated percepts and zerg bot

--- eisbw/src/main/java/eisbw/percepts/UnitPercept.java
+++ eisbw/src/main/java/eisbw/percepts/UnitPercept.java
@@ -10,14 +10,24 @@ public class UnitPercept extends Percept {
   private static final long serialVersionUID = 1L;
 
   /**
+   * Constructor UnitPercept.
+   * 
    * @param isFriendly
+   *          Indicates whether the unit is friendly or not
    * @param type
+   *          The unit type
    * @param id
+   *          The ID of the unit
    * @param health
+   *          The amount of health of the unit
    * @param shields
+   *          The amount of shields of the unit
    * @param isFlying
+   *          Indicates whether the unit is flying or not
    * @param bx
+   *          The x-coordinate of the unit
    * @param by
+   *          The y-coordinate of the unit
    */
   public UnitPercept(boolean isFriendly, String type, int id, int health, int shields,
       boolean isFlying, int bx, int by) {
