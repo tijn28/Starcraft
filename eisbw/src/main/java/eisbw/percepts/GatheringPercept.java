c17be89e5beaf43248ced6098c3f7c1c2fb03225
Dahny <P.C.plenge@student.tudelft.nl>
2016-05-20 12:07:25 +0200

    Added examplebot terran

---
+++ eisbw/src/main/java/eisbw/percepts/GatheringPercept.java
@@ -0,0 +1,14 @@
+package eisbw.percepts;
+
+import eis.iilang.Numeral;
+import eis.iilang.Percept;
+
+public class GatheringPercept extends Percept {
+
+  private static final long serialVersionUID = 1L;
+
+  public GatheringPercept(int id) {
+    super(Percepts.Gathering, new Numeral(id));
+  }
+
+}
